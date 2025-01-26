046262e94f56894d44eb7a49a7cf45e313ab1a8a
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2025-01-25 16:45:11 -0800

    sleepoutside wk3

--- src/js/product.js
+++ src/js/product.js
@@ -1,23 +1,5 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
-function addProductToCart(product) {
-  const existingCart = getLocalStorage("so-cart") || []; 
-
-  if (Array.isArray(existingCart)) {
-    existingCart.push(product);
-    setLocalStorage("so-cart", existingCart); 
-  } else {
-    console.error("Cart data in localStorage is corrupted. Resetting the cart.");
-    setLocalStorage("so-cart", [product]); 
-  }
-}
-
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productId = getParam("product");
+productDetails(productId);
