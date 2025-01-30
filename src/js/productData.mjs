dc40b80e98115ee4ee78a76bac5a0b6fd8a8b61b
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2025-01-29 16:23:50 -0800

    updated wk4

--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -9,10 +9,10 @@ function convertToJson(res) {
 export function getData(category = "tents") {
   return fetch(`../json/${category}.json`)
     .then(convertToJson)
-    .then(function(data) { return data; }); // Line 12 updated here
+    .then((data) => data);
 }
 
 export async function findProductById(id) {
   const products = await getData();
-  return products.find(function(item) { return item.Id === id; }); // Line 17 updated here
+  return products.find((item) => item.Id === id);
 }
